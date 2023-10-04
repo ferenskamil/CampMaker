e0c72d356aae8e63b806648327b32db2a930c5eb
Kamil <mr.kaam@gmail.com>
2023-10-04 10:03:46 +0200

    Feat: create table 'event' in DB

---
+++ src/Entity/Event.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $term_from = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $term_to = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $locality = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $price = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getTermFrom(): ?\DateTimeInterface
+    {
+        return $this->term_from;
+    }
+
+    public function setTermFrom(\DateTimeInterface $term_from): static
+    {
+        $this->term_from = $term_from;
+
+        return $this;
+    }
+
+    public function getTermTo(): ?\DateTimeInterface
+    {
+        return $this->term_to;
+    }
+
+    public function setTermTo(\DateTimeInterface $term_to): static
+    {
+        $this->term_to = $term_to;
+
+        return $this;
+    }
+
+    public function getLocality(): ?string
+    {
+        return $this->locality;
+    }
+
+    public function setLocality(string $locality): static
+    {
+        $this->locality = $locality;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
