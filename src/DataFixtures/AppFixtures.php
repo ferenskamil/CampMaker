488bd8339bb8e19b1992f7fb6f6579800f7d1b94
Kamil <mr.kaam@gmail.com>
2023-10-06 12:36:42 +0200

    Feat: load UserFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,22 +3,30 @@
 namespace App\DataFixtures;
 
 use App\DataFixtures\EventFixture;
+use App\DataFixtures\UserFixture;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager) : void {
-        $eventFixture = new EventFixture();
+    public function load(ObjectManager $manager) : void
+    {
+        /**Users */
+        $userFixture = new UserFixture ;
+        $usersFixtures = $userFixture->getUsersFixtures();
+        foreach ($usersFixtures as $user) {
+            /** @var \User $user */
+            $manager->persist($user);
+        }
 
         /** Events*/
+        $eventFixture = new EventFixture();
         $eventsData = $eventFixture->getEventsData();
         foreach ($eventsData as $event) {
            $event = $eventFixture->prepareEvent($event);
            $manager->persist($event);
         }
 
-        
 
         /**Flush */
         $manager->flush();
