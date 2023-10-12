8946d49788d6a00684059cdd9e97e2555944220b
Kamil <mr.kaam@gmail.com>
2023-10-12 09:13:05 +0200

    Feat: connect form with event

--- src/Controller/TripRegistrationController.php
+++ src/Controller/TripRegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\TripRegistration;
 use App\Form\TripRegistrationType;
+use App\Repository\EventRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,20 +13,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TripRegistrationController extends AbstractController
 {
-    #[Route('/zapisz-sie', name: 'trip_registration')]
+    #[Route('/zapisz-sie/{id}', name: 'trip_registration')]
     public function tripRegistartion(
+        int $id,
         Request $request,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        EventRepository $eventRepository
     ): Response {
         $tripRegistration = new TripRegistration();
+        $event = $eventRepository->find($id);
 
+
+
+        /** Handle form */
         $form = $this->createForm(TripRegistrationType::class, $tripRegistration);
-
         $form->handleRequest($request);
+
+        /** If user submitted form */
         if ($form->isSubmitted() && $form->isValid()) {
+            /** do right job */
             $form->getData();
 
             $newRegistration = $form->getData();
+            $newRegistration->setEvent($event);
             $entityManager->persist($newRegistration);
             $entityManager->flush();
 
@@ -33,7 +43,8 @@ class TripRegistrationController extends AbstractController
         }
 
         $params = [
-            'form' => $form
+            'form' => $form,
+            'event' => $event
         ];
         return $this->render('trip_registration/index.html.twig', $params);
     }
